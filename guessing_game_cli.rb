4778e407c7a095e80bf27c396059b372796f8810
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-18 23:30:58 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,12 +2,11 @@ def run_guessing_game
   num = rand(1..6).to_s
   input = gets
   
-  if input == ""
+  if input == 
     
     puts "You guessed the correct number!"
     elsif input != num
       puts "Sorry! The computer guessed #{num}."
-     elsif  input == "exit"
-      puts "Goodbye!"
+     elsif  
    end 
 end 
