c3034a8bb13661f527beaae0f72e10277cbd266e
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-18 23:36:09 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,7 +6,7 @@ def run_guessing_game
    if input == "exit"
       puts "Goodbye!"
    elsif input == num
-      puts ""
+      puts "You guessed the correct number!"
    else
       puts "Sorry! The computer guessed #{num}."
    end 
